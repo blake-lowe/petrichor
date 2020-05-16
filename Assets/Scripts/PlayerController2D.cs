81c041e5df7a98dd3df57fb15441e00bc7f30239
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-16 18:31:17 -0400

    Created Player movement script

---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
