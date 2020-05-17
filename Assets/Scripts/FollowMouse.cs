6ae152801bd39b76e1c59d47e620471eb6372b2b
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-17 16:20:04 -0400

    Changed camera to follow mouse!

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public float moveSpeed;
+    public Camera camera;
+    private Vector3 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        var targetPosition = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        transform.SetPositionAndRotation(targetPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/TargetLerp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLerp : MonoBehaviour
+{
+    public Transform transform1;
+    public Transform transform2;
+    public float weight;
+    void Update()
+    {
+        transform.SetPositionAndRotation(Vector3.Lerp(transform1.position, transform2.position, weight), 
+            Quaternion.identity);
+    }
+}
