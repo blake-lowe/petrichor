f4420af7ada4306aa01f10b06f60c0a930f8624d
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-18 22:08:28 -0400

    preliminary inventory ui

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    private string _name = "unassigned";
+    private string _description = "unassigned";
+
+    public string Name => _name;
+
+    public string Description => _description;
+}
