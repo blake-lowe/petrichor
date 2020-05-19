7e20a9811ad0aa7b99d87abdd8a09d3d8d47a3ef
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-18 21:36:50 -0400

    Created Enemy Controller script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class EnemyController : MonoBehaviour
+{
+    //public float speed;
+    public Animator animator;
+    
+    public Pathfinding.AIDestinationSetter aiDestinationSetter;
+    public Pathfinding.Patrol patrol;
+
+    public float alertRadius;
+    public float timeToNotice;
+    
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        
+    }
+}
