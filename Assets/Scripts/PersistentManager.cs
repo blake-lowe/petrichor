747d299ef7429cf2c1c6724b075cad40814c3186
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-20 17:04:40 -0400

    Added object persistence between scenes

---
+++ Assets/Scripts/PersistentManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentManager : MonoBehaviour
+{
+    public bool dontDestroyOnLoad = true;
+    private static PersistentManager _sInstance = null;
+
+    private void Awake()
+    {
+        if (_sInstance == null)
+        {
+            _sInstance = this;
+            if (dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(this);
+            }
+        }
+        else
+        { 
+            Destroy(gameObject);
+        }
+    }
+}
