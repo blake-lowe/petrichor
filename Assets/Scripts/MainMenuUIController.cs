6c00215c60a985a53c93e3e51d6378f73b129bed
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2020-05-24 20:53:08 -0400

    Added options menu to title screen

--- Assets/Scripts/MainMenuUIController.cs
+++ Assets/Scripts/MainMenuUIController.cs
@@ -1,41 +1,110 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenuUIController : MonoBehaviour
 {
     public int startGameSceneIndex;
-    public GameObject MainMenuCanvas;
-    public GameObject OptionsCanvas;
-    public GameObject CreditsCanvas;
-    public void doExitGame()//called by ui
+    public GameObject mainMenuCanvas;
+    public GameObject optionsCanvas;
+    public GameObject creditsCanvas;
+    public GameObject buildInfoCanvas;
+    public GameObject bindingsCanvas;
+    public AudioMixer audioMixer;
+    public TextMeshProUGUI volumeText;
+    public Slider volumeSlider;
+    public Dropdown resolutionDropdown;
+    
+    private Resolution[] _resolutions;
+    
+    private void Start()
+    {
+        volumeText.text = Mathf.RoundToInt(((volumeSlider.value + 80) / 80) * 100).ToString() + "%";
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        var currentResolutionIndex = 0;
+        for(int i = 0; i < _resolutions.Length; i++)
+        {
+            var option = _resolutions[i].width + "x" + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.width && 
+                _resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+            
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void DoExitGame()//called by ui
     {
         Debug.Log("Quitting");
         Application.Quit();
     }
 
-    public void doStartGame() //called by ui
+    public void DoStartGame() //called by ui
     {
         SceneManager.LoadScene(startGameSceneIndex);
     }
 
-    public void doOpenCredits()//called by ui
+    public void DoOpenCredits()//called by ui
     {
-        MainMenuCanvas.SetActive(false);
-        CreditsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
     }
 
-    public void doOpenOptions()//called by ui
+    public void DoOpenOptions()//called by ui
     {
-        MainMenuCanvas.SetActive(false);
-        OptionsCanvas.SetActive(true);
+        mainMenuCanvas.SetActive(false);
+        bindingsCanvas.SetActive(false);
+        optionsCanvas.SetActive(true);
+    }
+    
+    public void DoOpenBuildInfo()
+    {
+        mainMenuCanvas.SetActive(false);
+        buildInfoCanvas.SetActive(true);
     }
 
-    public void doOpenMainMenu()//called by ui
+    public void DoOpenBindings()
     {
-        OptionsCanvas.SetActive(false);
-        CreditsCanvas.SetActive(false);
-        MainMenuCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+        bindingsCanvas.SetActive(true);
+    }
+
+    public void DoOpenMainMenu()//called by ui
+    {
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
+        buildInfoCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+    }
+    
+    //options setters
+    public void SetVolume(float volume)
+    {
+        volumeText.text = Mathf.RoundToInt(((volume + 80) / 80) * 100).ToString() + "%";
+        audioMixer.SetFloat("Master Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
