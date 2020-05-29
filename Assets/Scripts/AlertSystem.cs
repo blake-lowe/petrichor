04528bf165f0fe9cb93f181e8245f78bd794d637
0x0wl <p4rrot@protonmail.com>
2020-05-29 01:15:40 -0400

    Alert System updated.

---
+++ Assets/Scripts/AlertSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertSystem : MonoBehaviour
+{
+
+	public bool AlarmTrigger = false;
+	private float TimeToDisableAlarm = 0;
+	public float AlarmDelay = 10;
+
+    // Start is called before the first frame update
+    /*
+    private void Start()
+    {
+        
+    }
+    */
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (AlarmTrigger == true)
+        {
+        	if (Time.time > TimeToDisableAlarm)
+        	{
+        		AlarmTrigger = false;
+        	}
+        }
+    }
+
+    public void SoundAlarm()
+    {
+    	AlarmTrigger = true;
+    	TimeToDisableAlarm = Time.time + AlarmDelay;
+    }
+}
